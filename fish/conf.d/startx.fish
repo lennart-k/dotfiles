82580310451e5ab1d9ad6c996330b6d4763c6d0c
Lennart <18233294+lennart-k@users.noreply.github.com>
2023-09-28 17:07:56 +0200

    fish: dumb bugfix where it sometimes was not possible to open a new tmux pane over ssh

--- fish/conf.d/startx.fish
+++ fish/conf.d/startx.fish
@@ -1,4 +1,7 @@
+# https://wiki.archlinux.org/title/fish#Start_X_at_login
+# *: Really dumb fix, if the tmux session is opened on the host then fish will crash when trying to open a new pane via SSH
 if status is-login
+    and not set -q TMUX # *
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
         exec startx -- -keeptty
     end
