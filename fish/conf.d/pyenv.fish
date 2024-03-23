8f574b3790f7323aa055ab575c917796d098e432
Lennart <18233294+lennart-k@users.noreply.github.com>
2024-03-23 17:46:47 +0100

    fish: pyenv

--- fish/conf.d/pyenv.fish
+++ fish/conf.d/pyenv.fish
@@ -1,3 +1,20 @@
 if command -sq pyenv
-    pyenv init - | source
+    while set pyenv_index (contains -i -- "/home/$USER/.pyenv/shims" $PATH)
+        set -eg PATH[$pyenv_index]
+    end
+    set -e pyenv_index
+    set -gx PATH "/home/$USER/.pyenv/shims" $PATH
+    set -gx PYENV_SHELL fish
+    command pyenv rehash 2>/dev/null
+    function pyenv
+        set command $argv[1]
+        set -e argv[1]
+
+        switch "$command"
+            case rehash shell
+                source (pyenv "sh-$command" $argv|psub)
+            case '*'
+                command pyenv "$command" $argv
+        end
+    end
 end
